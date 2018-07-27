1f3ae0c9ecc3fbc0f17e5ddf290e268291c4da98
amoscatelli <alessandro.moscatelli@live.com>
2018-07-27 20:44:07 +0200

    visualizzazione 2d

--- Visiontech.Analyzer/ViewModels/ViewModel.cs
+++ Visiontech.Analyzer/ViewModels/ViewModel.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -24,6 +23,8 @@ namespace VisualizzatoreWPF.ViewModels
 
         public ICommand LoadFileCommand { get; }
 
+        public event EventHandler<Tuple<Side, analyzeLensResponseDTO>> LensAnalyzed;
+
         public ViewModel()
         {
             LoadFileCommand = new Command((parameter) => LoadFileAsync(parameter as Tuple<Side, string[]>), (parameter) => !(parameter is null) && parameter is Tuple<Side, string[]>  && (parameter as Tuple<Side, string[]>).Item2.Length > 0);
@@ -40,54 +41,221 @@ namespace VisualizzatoreWPF.ViewModels
                 case ".txt":
                 case ".xyz":
 
-                    using (StreamReader streamReader = new StreamReader(tuple.Item2[0]))
-                    {
-
-                        string line;
-
-                        while ((line = streamReader.ReadLine()) != null)
-                        {
-                            string[] coordinates = line.Split(' ');
-                            if (coordinates.Length == 3) {
-                                points.Add(new threeDimensionalPointDTO() {
-                                    x = double.Parse(coordinates[0], CultureInfo.InvariantCulture),
-                                    xSpecified = true,
-                                    y = double.Parse(coordinates[1], CultureInfo.InvariantCulture),
-                                    ySpecified = true,
-                                    z = double.Parse(coordinates[2], CultureInfo.InvariantCulture),
-                                    zSpecified = true
-                                });
-                            }
-                        }
-
-                        if (points.Count > 0)
-                        {
-
-                            analyzeLensRequestDTO.points = points.ToArray();
-
-                            computeResponseDTO computeResponseDTO = computeSoapClient.analyzeLens(analyzeLensRequestDTO);
-
-                            if (computeResponseDTO is analyzeLensResponseDTO)
-                            {
-                                foreach (analyzedPointDTO point in (computeResponseDTO as analyzeLensResponseDTO).points)
-                                {
-                                    Debug.WriteLine("CICCIO " + string.Join(" ", point.x.ToString(), point.y.ToString(), point.z.ToString(), point.powerMap.ToString(), point.cylinderMap.ToString()));
-                                }
-                            }
-
-                        }
-
-                    }
+                    Task.Run(() => LensAnalyzed.Invoke(this, new Tuple<Side, analyzeLensResponseDTO>(tuple.Item1, computeSoapClient.analyzeLens(FromXYZFile(tuple.Item2[0])) as analyzeLensResponseDTO)));
 
                     break;
                 case ".hmf":
-                    Debug.WriteLine("1");
+
+                    Task.Run(() => LensAnalyzed.Invoke(this, new Tuple<Side, analyzeLensResponseDTO>(tuple.Item1, computeSoapClient.analyzeLens(FromHMFFile(tuple.Item2[0])) as analyzeLensResponseDTO)));
+
                     break;
                 case ".sdf":
-                    Debug.WriteLine("2");
+
+                    Tuple<analyzeLensRequestDTO, analyzeLensRequestDTO> requests = FromSDFFile(tuple.Item2[0]);
+                    if (requests.Item1 != null)
+                    {
+                        Task.Run(() => LensAnalyzed.Invoke(this, new Tuple<Side, analyzeLensResponseDTO>(Side.LEFT, computeSoapClient.analyzeLens(requests.Item1) as analyzeLensResponseDTO)));
+                    }
+                    if (requests.Item2 != null)
+                    {
+                        Task.Run(() => LensAnalyzed.Invoke(this, new Tuple<Side, analyzeLensResponseDTO>(Side.RIGHT, computeSoapClient.analyzeLens(requests.Item2) as analyzeLensResponseDTO)));
+                    }
+
                     break;
             }
 
         }
+
+        private analyzeLensRequestDTO FromXYZFile(string file)
+        {
+
+            analyzeLensRequestDTO analyzeLensRequestDTO = new analyzeLensRequestDTO();
+            ICollection<threeDimensionalPointDTO> points = new Collection<threeDimensionalPointDTO>();
+
+            using (StreamReader streamReader = new StreamReader(file))
+            {
+
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    string[] coordinates = line.Split(' ');
+                    if (coordinates.Length == 3)
+                    {
+                        points.Add(new threeDimensionalPointDTO()
+                        {
+                            x = double.Parse(coordinates[0], CultureInfo.InvariantCulture),
+                            xSpecified = true,
+                            y = double.Parse(coordinates[1], CultureInfo.InvariantCulture),
+                            ySpecified = true,
+                            z = double.Parse(coordinates[2], CultureInfo.InvariantCulture),
+                            zSpecified = true
+                        });
+                    }
+                }
+
+            }
+
+            analyzeLensRequestDTO.points = points.ToArray();
+
+            return analyzeLensRequestDTO;
+
+        }
+
+        private analyzeLensRequestDTO FromHMFFile(string file)
+        {
+
+            analyzeLensRequestDTO analyzeLensRequestDTO = null;
+            Collection<Collection<threeDimensionalPointDTO>> rows = new Collection<Collection<threeDimensionalPointDTO>>();
+
+            int Count = 0;
+            double Interval = 0;
+
+            using (StreamReader streamReader = new StreamReader(file))
+            {
+
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("Count")) {
+                        Count = int.Parse(line.Split('=')[1], CultureInfo.InvariantCulture);
+                    } else if (line.StartsWith("Interval"))
+                    {
+                        Interval = double.Parse(line.Split('=')[1], CultureInfo.InvariantCulture);
+                    } else if ("[Data]".Equals(line))
+                    {
+                        while ((line = streamReader.ReadLine()) != null)
+                        {
+                            Collection<threeDimensionalPointDTO> row = new Collection<threeDimensionalPointDTO>();
+                            foreach (string z in line.Split(','))
+                            {
+                                row.Add(
+                                    new threeDimensionalPointDTO(){
+                                        z = double.Parse(z, CultureInfo.InvariantCulture),
+                                        zSpecified = true
+                                    }
+                                );
+                            }
+                            rows.Add(row);
+                        }
+                    }
+                }
+
+            }
+
+            if (Count > 0 && Interval > 0 && Count == rows.Count) {
+                analyzeLensRequestDTO = new analyzeLensRequestDTO
+                {
+                    points = ComputeXY(rows, Count, Interval).ToArray()
+                };
+            }
+
+            return analyzeLensRequestDTO;
+
+        }
+
+
+
+        private Tuple<analyzeLensRequestDTO, analyzeLensRequestDTO> FromSDFFile(string file)
+        {
+
+            analyzeLensRequestDTO leftAnalyzeLensRequestDTO = null;
+            analyzeLensRequestDTO rightAnalyzeLensRequestDTO = null;
+            Collection<Collection<threeDimensionalPointDTO>> leftRows = null;
+            Collection<Collection<threeDimensionalPointDTO>> rightRows = null;
+            Collection<Collection<threeDimensionalPointDTO>> currentRows = null;
+
+            int leftCount = 0;
+            int rightCount = 0;
+            double leftInterval = 0;
+            double rightInterval = 0;
+
+            using (StreamReader streamReader = new StreamReader(file))
+            {
+
+                string line;
+
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("SURFMT"))
+                    {
+                        string[] parameters = line.Split('=')[1].Split(';');
+
+                        currentRows = new Collection<Collection<threeDimensionalPointDTO>>();
+
+                        if ("R".Equals(parameters[1]))
+                        {
+                            rightInterval = double.Parse(parameters[0], CultureInfo.InvariantCulture);
+                            rightCount = int.Parse(parameters[3], CultureInfo.InvariantCulture);
+                            rightRows = currentRows;
+                        } else
+                        {
+                            leftInterval = double.Parse(parameters[0], CultureInfo.InvariantCulture);
+                            leftCount = int.Parse(parameters[3], CultureInfo.InvariantCulture);
+                            leftRows = currentRows;
+                        }
+
+                    } else if (line.StartsWith("ZZ") && currentRows != null)
+                    {
+                        Collection<threeDimensionalPointDTO> row = new Collection<threeDimensionalPointDTO>();
+                        foreach (string z in line.Split('=')[1].Split(';'))
+                        {
+                            row.Add(
+                                new threeDimensionalPointDTO()
+                                {
+                                    z = double.Parse(z, CultureInfo.InvariantCulture),
+                                    zSpecified = true
+                                }
+                            );
+                        }
+                        currentRows.Add(row);
+                    }
+                }
+
+            }
+
+            if (leftCount > 0 && leftInterval > 0 && leftCount == leftRows.Count)
+            {
+                leftAnalyzeLensRequestDTO = new analyzeLensRequestDTO
+                {
+                    points = ComputeXY(leftRows, leftCount, leftInterval).ToArray()
+                };
+            }
+
+            if (rightCount > 0 && rightInterval > 0 && rightCount == rightRows.Count)
+            {
+                rightAnalyzeLensRequestDTO = new analyzeLensRequestDTO
+                {
+                    points = ComputeXY(rightRows, rightCount, rightInterval).ToArray()
+                };
+            }
+
+            return new Tuple<analyzeLensRequestDTO, analyzeLensRequestDTO>(leftAnalyzeLensRequestDTO, rightAnalyzeLensRequestDTO);
+
+        }
+
+        private Collection<threeDimensionalPointDTO> ComputeXY(Collection<Collection<threeDimensionalPointDTO>> rows, int Count, double Interval) {
+
+
+            Collection<threeDimensionalPointDTO> points = new Collection<threeDimensionalPointDTO>();
+
+            foreach (Collection<threeDimensionalPointDTO> row in rows)
+            {
+                foreach (threeDimensionalPointDTO point in row)
+                {
+                    point.x = Interval * row.IndexOf(point) - (Count - 1) / 2.0 * Interval;
+                    point.xSpecified = true;
+                    point.y = (Count - 1) / 2.0 * Interval - Interval * rows.IndexOf(row);
+                    point.ySpecified = true;
+
+                    points.Add(point);
+                }
+            }
+
+            return points;
+
+        }
+
     }
 }
--- Visiontech.Analyzer/ViewPage.xaml.cs
+++ Visiontech.Analyzer/ViewPage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Org.Visiontech.Compute;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using SkiaSharp.Views.WPF;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using VisiontechCommons;
@@ -16,12 +21,196 @@ namespace VisualizzatoreWPF
         public ViewPage()
         {
             InitializeComponent();
+
+            model.LensAnalyzed += Model_LensAnalyzed;
         }
 
-        private void StackPanel_Drop(object sender, DragEventArgs e)
+        private void Model_LensAnalyzed(object sender, Tuple<ViewModel.Side, analyzeLensResponseDTO> tuple)
         {
-            ViewModel.Side side = RightStackPanel.Equals(sender) ? ViewModel.Side.RIGHT : ViewModel.Side.LEFT;
+            
+            switch (tuple.Item1)
+            {
+                case ViewModel.Side.LEFT:
+                    Dispatcher.Invoke(() => {
+                        LeftLens.DataContext = tuple.Item2;
+                        LeftLens.InvalidateVisual();
+                    });
+                    break;
+                case ViewModel.Side.RIGHT:
+                    Dispatcher.Invoke(() => {
+                        RightLens.DataContext = tuple.Item2;
+                        RightLens.InvalidateVisual();
+                    });
+                    break;
+            }
+
+        }
+
+        private void DropFile(object sender, DragEventArgs e)
+        {
+            ViewModel.Side side = RightLens.Equals(sender) ? ViewModel.Side.RIGHT : ViewModel.Side.LEFT;
             model.LoadFileCommand.Execute(new Tuple<ViewModel.Side, string[]>(side, e.Data.GetData(DataFormats.FileDrop) as string[]));
+        }
+
+        private void PaintLens(object sender, SKPaintSurfaceEventArgs args)
+        {
+
+            if ((sender as SKElement).DataContext != null && (sender as SKElement).DataContext is analyzeLensResponseDTO)
+            {
+
+                analyzeLensResponseDTO analyzeLensResponseDTO = (sender as SKElement).DataContext as analyzeLensResponseDTO;
+
+                SKImageInfo info = args.Info;
+                SKSurface surface = args.Surface;
+                SKCanvas canvas = surface.Canvas;
+
+                canvas.Clear();
+
+                SKPaint blackSKPaint = new SKPaint
+                {
+                    Style = SKPaintStyle.Fill,
+                    Color = SKColors.Black
+                };
+
+                canvas.DrawRect(new SKRect(0, 0, info.Width, info.Height), blackSKPaint);
+
+                if (analyzeLensResponseDTO.points != null & analyzeLensResponseDTO.points.Any())
+                {
+
+                    double radius = Math.Max(analyzeLensResponseDTO.points.Select(point => Math.Abs(point.x)).Max(), analyzeLensResponseDTO.points.Select(point => Math.Abs(point.y)).Max());
+
+                    ICollection<analyzedPointDTO> points = analyzeLensResponseDTO.points.Where(point => Math.Sqrt(Math.Pow(point.x, 2) + Math.Pow(point.y, 2)) <= radius).Select(point => point as analyzedPointDTO).ToList();
+
+                    double maxCylinder = points.Select(point => point.cylinderMap).Max();
+                    double minCylinder = points.Select(point => point.cylinderMap).Min();
+                    double cylinderRange = (maxCylinder - minCylinder) / 6.0;
+
+                    if (cylinderRange > 0)
+                    {
+
+                        double doubleCylinderRange = cylinderRange * 2;
+                        double tripleCylinderRange = cylinderRange * 3;
+                        double fiveCylinderRange = cylinderRange * 5;
+
+                        double centerX = info.Width / 2;
+                        double centerY = info.Height / 2;
+
+                        int sqrt = Convert.ToInt32(Math.Sqrt(points.Count));
+                        int side = (sqrt - 1) / 2;
+
+                        double bitmapSide = Math.Min(info.Width, info.Height) / 2.0;
+
+                        double sqrtRange = sqrt / 6.0;
+                        double doubleSqrtRange = sqrtRange * 2;
+                        double tripleSqrtRange = sqrtRange * 3;
+                        double fiveSqrtRange = sqrtRange * 5;
+
+                        using (SKBitmap bitmap = new SKBitmap(1, sqrt))
+                        {
+
+                            for (int i = sqrt - 1; i >= 0; i--)
+                            {
+
+                                SKColor color = SKColors.Black;
+
+                                switch (Math.Floor(i / sqrtRange))
+                                {
+
+                                    case 0:
+                                        color = new SKColor(Convert.ToByte(Math.Round(i / sqrtRange * 255)), Convert.ToByte(Math.Round(i / sqrtRange * 255)), 255);
+                                        break;
+                                    case 1:
+                                        color = new SKColor(Convert.ToByte(Math.Round((doubleSqrtRange - i) / sqrtRange * 255)), 255, 255);
+                                        break;
+                                    case 2:
+                                        color = new SKColor(0, 255, Convert.ToByte(Math.Round((tripleSqrtRange - i) / sqrtRange * 255)));
+                                        break;
+                                    case 3:
+                                        color = new SKColor(Convert.ToByte(Math.Round((i - tripleSqrtRange) / sqrtRange * 255)), 255, 0);
+                                        break;
+                                    case 4:
+                                        color = new SKColor(255, Convert.ToByte(Math.Round((fiveSqrtRange - i) / sqrtRange * 255)), 0);
+                                        break;
+                                    default:
+                                        color = new SKColor(255, 0, Convert.ToByte(Math.Round((i - fiveSqrtRange) / sqrtRange * 255)));
+                                        break;
+
+                                }
+
+                                bitmap.SetPixel(0, i, color);
+
+                            }
+
+                            canvas.DrawBitmap(bitmap.Resize(new SKImageInfo(Convert.ToInt32(bitmapSide / 6), Convert.ToInt32(bitmapSide)), SKBitmapResizeMethod.Mitchell), new SKRect(Convert.ToSingle(info.Width - bitmapSide / 6), 0, Convert.ToSingle(info.Width), info.Height));
+
+                        }
+
+                        blackSKPaint.TextSize = info.Height / 32;
+
+                        float textRange = (info.Height - blackSKPaint.TextSize * 3) / 6;
+
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder + cylinderRange), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder + cylinderRange * 2), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange * 2, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder + cylinderRange * 3), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange * 3, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder + cylinderRange * 4), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange * 4, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", minCylinder + cylinderRange * 5), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange * 5, blackSKPaint);
+                        canvas.DrawText(string.Format("{0:N2}", maxCylinder), Convert.ToSingle(info.Width - bitmapSide / 8), blackSKPaint.TextSize * 2 + textRange * 6, blackSKPaint);
+
+                        using (SKBitmap bitmap = new SKBitmap(sqrt, sqrt))
+                        {
+
+                            foreach (analyzedPointDTO point in points)
+                            {
+
+                                SKColor color = SKColors.Black;
+
+                                double value = point.cylinderMap - minCylinder;
+
+                                switch (Math.Floor(value / cylinderRange))
+                                {
+
+                                    case 0:
+                                        color = new SKColor(Convert.ToByte(Math.Round(value / cylinderRange * 255)), Convert.ToByte(Math.Round(value / cylinderRange * 255)), 255);
+                                        break;
+                                    case 1:
+                                        color = new SKColor(Convert.ToByte(Math.Round((doubleCylinderRange - value) / cylinderRange * 255)), 255, 255);
+                                        break;
+                                    case 2:
+                                        color = new SKColor(0, 255, Convert.ToByte(Math.Round((tripleCylinderRange - value) / cylinderRange * 255)));
+                                        break;
+                                    case 3:
+                                        color = new SKColor(Convert.ToByte(Math.Round((value - tripleCylinderRange) / cylinderRange * 255)), 255, 0);
+                                        break;
+                                    case 4:
+                                        color = new SKColor(255, Convert.ToByte(Math.Round((fiveCylinderRange - value) / cylinderRange * 255)), 0);
+                                        break;
+                                    default:
+                                        color = new SKColor(255, 0, Convert.ToByte(Math.Round((value - fiveCylinderRange) / cylinderRange * 255)));
+                                        break;
+
+                                }
+
+                                bitmap.SetPixel(Convert.ToInt32(side - point.x), Convert.ToInt32(side - point.y), color);
+
+                            }
+
+                            using (SKPath path = new SKPath())
+                            {
+                                path.AddCircle(Convert.ToSingle(centerX), Convert.ToSingle(centerY), Convert.ToSingle(bitmapSide * 5 / 6));
+                                canvas.ClipPath(path, SKClipOperation.Intersect);
+                            }
+
+                            canvas.DrawBitmap(bitmap.Resize(new SKImageInfo(Convert.ToInt32(bitmapSide * 5 / 6), Convert.ToInt32(bitmapSide * 5 / 6)), SKBitmapResizeMethod.Mitchell), new SKRect(Convert.ToSingle(centerX - bitmapSide * 5 / 6), Convert.ToSingle(centerY - bitmapSide * 5 / 6), Convert.ToSingle(centerX + bitmapSide * 5 / 6), Convert.ToSingle(centerY + bitmapSide * 5 / 6)));
+
+                        }
+
+                    }
+
+                }
+
+            }
+
         }
     }
 }
